de76c13ff8a983bfd9b8825334e65005b7fd13da
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-09-30 22:16:05 +0530

    changes commmit

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,33 +1,48 @@
-// routes/todoRoutes.js
 const express = require('express');
 const Todo = require('../models/Todo');
 const router = express.Router();
 
 // Get all todos
 router.get('/', async (req, res) => {
-  const todos = await Todo.find();
-  res.json(todos);
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching todos', error });
+  }
 });
 
 // Create a new todo
 router.post('/', async (req, res) => {
-  const newTodo = new Todo({
-    task: req.body.task,
-  });
-  await newTodo.save();
-  res.json(newTodo);
+  try {
+    const newTodo = new Todo({
+      task: req.body.task,
+    });
+    await newTodo.save();
+    res.status(201).json(newTodo);
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating todo', error });
+  }
 });
 
 // Update a todo
 router.put('/:id', async (req, res) => {
-  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(updatedTodo);
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedTodo);
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating todo', error });
+  }
 });
 
 // Delete a todo
 router.delete('/:id', async (req, res) => {
-  await Todo.findByIdAndDelete(req.params.id);
-  res.json({ message: 'Todo deleted' });
+  try {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Todo deleted' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting todo', error });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const todoRoutes = require('./routes/todoRoutes');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://mern-todo-frontend-flax.vercel.app', 'http://localhost:3000'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, 
+}));
 app.use(express.json());
-
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+  .catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/api/todos', todoRoutes);
 
